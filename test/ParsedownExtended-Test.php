436e1789af4c1edc6bd07bfa8f3e328d19427d5c
Simon Christmann <simon@christmann.email>
2016-02-14 21:33:54 +0100

    Cosmetics for Debug Output

--- test/ParsedownExtended-Test.php
+++ test/ParsedownExtended-Test.php
@@ -10,9 +10,10 @@ if(isset($_GET["file"])) {
 }
 $text = file_get_contents($file);
 
+echo "<pre>"; // better formatting of debug messages
 $ToC = new ParsedownExtended();
 $markdown = $ToC->text($text);
-
+echo "</pre>";
 
 echo $ToC->tableOfContentsList();
 
